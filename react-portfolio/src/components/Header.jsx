9724e54167ee6939d23abebfbdfaf9fa0e6c5d06
Sharon Moegi <sharamoegi@gmail.com>
2026-05-05 09:21:27 +0300

    Update Header

--- react-portfolio/src/components/Header.jsx
+++ react-portfolio/src/components/Header.jsx
@@ -3,8 +3,13 @@ import React from "react";
 function Header (){
     return(
         <header>
-            Header!
+           <h1 className="header">Personal Project Showcase App</h1> 
+           <nav>
+            <a className="nav-link" href="#work">Work</a>
+            <a className="nav-link" href="#about">About</a>
+            <a className="nav-link" href="#contact">Contact</a>
+           </nav>
         </header>
-    )
+    );
 }
 export default Header;
