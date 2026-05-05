f444ce0b7cbab074892f2bf737aeee0da2b53719
Sharon Moegi <sharamoegi@gmail.com>
2026-05-05 13:43:33 +0300

    Update Class names for the search bar

--- react-portfolio/src/components/SearchBar.jsx
+++ react-portfolio/src/components/SearchBar.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 function SearchBar ({value, onChange }) {
     return(
-        <div>
+        <div className="search-wrap">
             <span className="search-icon" aria-hidden="true">⌕</span>
-            <input type="text" placeholder="Search Projects" value={value} onChange={(e) => onChange(e.target.value)} />
+            <input className="search-input" type="text" placeholder="Search Projects" value={value} onChange={(e) => onChange(e.target.value)} />
             {value && (<button className="search-clear"
           onClick={() => onChange("")}
           aria-label="Clear search">
