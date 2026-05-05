a5cc245ae57fa87e54c5de42c32d41a9fd6c1272
Sharon Moegi <sharamoegi@gmail.com>
2026-05-05 09:15:58 +0300

    Create ProjectList function template

---
+++ react-portfolio/src/components/ProjectList.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ProjectCard from "./ProjectCard";
+
+function ProjectList(){
+    return(
+        <div>
+            Projects List
+        </div>
+    )
+}
+export default ProjectList;
