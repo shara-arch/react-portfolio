9a2cf12268cfbc1d4b35b4a7ac7d2a39faa5142c
Sharon Moegi <sharamoegi@gmail.com>
2026-05-05 11:56:16 +0300

    Initialize project card

--- react-portfolio/src/components/ProjectCard.jsx
+++ react-portfolio/src/components/ProjectCard.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 
-function ProjectCard(){
+function ProjectCard({project, index}){
+    const {title, description, year, client} = project;
     return(
-        <div>
-            Project Card !
+        <div className="project-card">
+            <h3>{title}</h3> 
+            <strong>{year}</strong>
+            <span>Client:</span><span id="project-client"> {client}</span>   
+            <p id="project-description">{description}</p>
+
         </div>
     )
 }
